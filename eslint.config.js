3c8d73419a0819783518dacb74b71d4b9b1ef766
MathysGbs <131974039+MathysGbs@users.noreply.github.com>
2026-09-11 14:24:40 +0200

    mise en place du lint

--- eslint.config.js
+++ eslint.config.js
@@ -1,3 +1,5 @@
+const globals = require('globals');
+
 module.exports = [
   {
     ignores: ['node_modules/**']
@@ -6,13 +8,27 @@ module.exports = [
     files: ['**/*.js'],
     languageOptions: {
       ecmaVersion: 2022,
-      sourceType: 'commonjs'
+      sourceType: 'commonjs',
+      globals: {
+        ...globals.node,
+        fetch: 'readonly' // Autorise fetch (disponible nativement depuis Node 18)
+      }
     },
     rules: {
       'no-unused-vars': 'error',
       'no-undef': 'error',
       'semi': ['error', 'always'],
       'quotes': ['error', 'single']
+    }
+  },
+  {
+    // Cible spécifiquement tes fichiers de tests
+    files: ['tests/**/*.js'],
+    languageOptions: {
+      globals: {
+        ...globals.node,
+        ...globals.jest // Remplace par globals.mocha si tu utilises Mocha
+      }
     }
   }
 ];
